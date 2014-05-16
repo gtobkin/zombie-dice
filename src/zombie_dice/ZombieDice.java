2f72570b084649a2b7cb9f27617210d0d6acaf36
Greg Tobkin <greg.tobkin@gmail.com>
2014-05-16 01:16:58 -0400

    Start looking at learning base (end-of-game) cases

--- src/zombie_dice/ZombieDice.java
+++ src/zombie_dice/ZombieDice.java
@@ -14,20 +14,27 @@ public class ZombieDice {
 	public static final int SHOTGUNS_TO_LOSE = 3;
 	public static final int HAND_SIZE = 3;
 	
-	public static final int WINNING_SCORE = 13;
+	public static final int WINNING_SCORE = 5;
 	
 	// ==================== METHODS ====================
 	
 	public static void main(String[] args) {
+		//computeBrainDistribution(10, 1000);
+		Strategy strat = new Strategy(3);
+		strat.learnBaseCases();
+	}
+	
+	public static void computeBrainDistribution(int samples, int turnsPerSample) {
+		final int MAX_BRAINS = 50;
 		GameState game = new GameState(1, 0, 0);
-		for (int i = 0; i < 20; ++i) {
-			int[] scores = new int[100];
-			for (int j = 0; j < 50000000; ++j) {
+		for (int i = 0; i < samples; ++i) {
+			int[] scores = new int[MAX_BRAINS + 1];
+			for (int j = 0; j < turnsPerSample; ++j) {
 				TurnState turn = new TurnState(game);
 				while (turn.shotguns() < 3) turn.roll();
 				++scores[turn.brains()];
 			}
-			for (int j = 0; j != 100; ++j) {
+			for (int j = 0; j != MAX_BRAINS + 1; ++j) {
 				System.out.print(scores[j] + "\t");
 			}
 			System.out.print("\n");
