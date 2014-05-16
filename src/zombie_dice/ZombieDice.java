3c5420245617c653e026c9a75ac6fd032f4b7a95
Greg Tobkin <greg.tobkin@gmail.com>
2014-05-15 22:03:30 -0400

    Add TurnState

--- src/zombie_dice/ZombieDice.java
+++ src/zombie_dice/ZombieDice.java
@@ -1,7 +1,15 @@
 package zombie_dice;
 
 public class ZombieDice {
-
+	
+	// ==================== CONSTANTS ====================
+	
+	public static final int DEFAULT_GREEN_DICE = 6;
+	public static final int DEFAULT_YELLOW_DICE = 4;
+	public static final int DEFAULT_RED_DICE = 3;
+	
+	public static final int WINNING_SCORE = 13;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
